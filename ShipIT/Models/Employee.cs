6702d9c996dc38cb39264c56d3c4d859a686fb1c
calhooligan <calhooligan@hotmail.com>
2017-01-29 01:21:07 -0500

    Foundational classes, data (JSON), and two sample views.

---
+++ ShipIT/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipIT.Models
+{
+    class Employee
+    {
+        #region Employee Members
+
+        public string EmployeeID { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string Department { get; set; }
+
+        #endregion
+    }
+}
---
+++ ShipIT/Models/Shipment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipIT.Models
+{
+    class Shipment //: IDataErrorInfo
+    {
+        #region Shipment Members
+
+        public int TrackingNum { get; set; }
+
+        public DateTime DateShipped { get; set; }
+
+        //public string Item { get; set; } ?
+
+        //public string ItemDescription { get; set; } ?
+
+        public string OriginatorName { get; set; }
+
+        public string OriginatorDept { get; set; }
+
+        public string DestinationName { get; set; }
+
+        public string DestinationDept { get; set; }
+
+        #endregion
+    }
+}
